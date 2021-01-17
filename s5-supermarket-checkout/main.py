8ef9c52ece8ce549dc18d2ee93620bda27e91af0
6tigersrds <6tigersrds@gmail.com>
2021-01-17 15:02:59 +0000

    complete section 5

--- s5-supermarket-checkout/main.py
+++ s5-supermarket-checkout/main.py
@@ -1,6 +1,15 @@
 from Checkout import Checkout
 
 checkout = Checkout()
-checkout.addItemPrice("a",1)
+
+checkout.addItemPrice("a", 1)
+checkout.addItemPrice("b", 2)
+
+checkout.addDiscount("a", 3, 2)
+
 checkout.addItem("a")
-print(checkout.calculateTotal())
+checkout.addItem("a")
+checkout.addItem("a")
+
+print("==================== [TOTAL] ====================")
+print("Your checkout total is: ", checkout.calculateTotal())
--- s5-supermarket-checkout/test_checkout.py
+++ s5-supermarket-checkout/test_checkout.py
@@ -30,3 +30,7 @@ def test_canApplyDiscountRule(checkout):
     checkout.addItem("a")
     checkout.addItem("a")
     assert checkout.calculateTotal() == 2
+
+def test_execeptionWithBadItem(checkout):
+    with pytest.raises(Exception):
+        checkout.addItem("c")
